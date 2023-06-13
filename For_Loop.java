ec52062e68897798c45db3b33bc90cebbdcfa89f
Nicolo' Ferri <irr3f92@gmail.com>
2023-06-13 14:34:45 +0200

    Esercizio 12

---
+++ For_Loop.java
@@ -0,0 +1,10 @@
+package co.develhope.introduction._12;
+
+public class For_Loop {
+
+    public static void main(String[] args) {
+        for ( int i=5 ; i<=25 ; i++){
+            System.out.println(i);
+        }
+    }
+}
